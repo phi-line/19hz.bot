9dc3f44ac0905b38bb13f249e0666496c224391f
phi-line <phi.liney@gmail.com>
2018-02-01 15:26:39 -0800

    removd data from repo

--- data.py
+++ data.py
@@ -1,24 +1,31 @@
 import csv
 import requests
 from urllib import parse
+import json
 
 DATA_PATH = 'data/'  # local path
-DATA_URL = 'https://19hz.info/' # online base
-DATA_FILES = ['events_BayArea.csv']  # filename
+DATA_URL = 'https://19hz.info/'  # online base
+DATA_FILES = ['events_BayArea']  # filename
 DATA_HEADERS = ["date", "name", "genre", 'location', 'time', 'price',
                 'ages', 'promoter', 'url1', 'url2', 'datetime']
 
 def fetch_data():
     for u in DATA_FILES:
-        csv_url = parse.urljoin(DATA_PATH, u)
-        with requests.Session() as s:
-            download = s.get(csv_url)
+        csv_url = parse.urljoin(DATA_URL, u + '.csv')
+        csv_local_path = parse.urljoin(DATA_PATH, u + '.csv')
+        json_local_path = parse.urljoin(DATA_PATH, u + '.json')
 
-            decoded_content = download.content.decode('utf-8')
+        data = requests.get(csv_url)
 
-            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-            my_list = list(cr)
-            for row in my_list
+        entry_list = list()
+        reader = csv.reader(data.text.splitlines())
+
+        for row in reader:
+            entry = dict(zip(DATA_HEADERS, row))
+            entry_list.append(entry)
+
+        json.dump(entry_list, open(json_local_path, 'w'), indent=4)
+        # data = json.load(open(json_local_path))
     return
 
 def update_data():
@@ -39,4 +46,5 @@ def update_data():
 
 
 if __name__ == '__main__':
-    update_data()
+    fetch_data()
+    # update_data()
