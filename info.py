1cad9db5381bf60502e07bd6d40dde2c996f7978
phi-line <phi.liney@gmail.com>
2018-02-01 20:01:19 -0800

    random colors and emojis

--- info.py
+++ info.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 import api
-from config import COLOR
+# from config import COLOR
+
+from random import randint
+from os.path import dirname
 
 # import maya
 
@@ -25,11 +28,17 @@ class Info():
         Usage: !event [location]
         e.g:   !event Bay Area
         '''
-        data = dotdict(api.fetch_data(args[0])[0])
+        dump = api.fetch_data(args[0])
+        random_num = randint(1, len(dump))
+        data = dotdict(dump[random_num])
 
-        embed = discord.Embed(title=data.name, url=(data.url1 or data.url2 or "19hz.info/#"), color=COLOR)
-        if data.ur1 and data:
-            embed.set_author(name="Extra Link", url=data.url2)
+        r = lambda: randint(0, 255)
+        hexc = int('0x{:02X}{:02X}{:02X}'.format(r(), r(), r()), 16)
+
+        embed = discord.Embed(title=data.name, url=(data.url2 or data.url1 or "19hz.info/#"), color=hexc)
+        if data.url1 and data.url2:
+            embed.set_author(name=data.name, url=data.url1)
+            embed.title = "Extra Link"
         embed = Info.info_embed(embed, data)
 
         return await self.bot.say(embed=embed)
@@ -37,13 +46,13 @@ class Info():
     @staticmethod
     def info_embed(embed: discord.Embed, data):
         genre = '`{}`'.format(na(data.genre))
-        embed.add_field(name="__Genre(s)__", value=genre, inline=False)
+        embed.add_field(name=":musical_note: __Genre(s)__", value=genre, inline=False)
 
         time = '{}\n`{}`'.format(na(data.date), na(data.time))
-        embed.add_field(name="__Time__", value=time, inline=True)
+        embed.add_field(name=":alarm_clock: __Time__", value=time, inline=True)
 
         info = 'Location: `{}`\nPrice: `{}`\nAges: `{}`'.format(na(data.location), na(data.price), na(data.ages))
-        embed.add_field(name="__Info__", value=info, inline=True)
+        embed.add_field(name=":grey_question: __Info__", value=info, inline=True)
         return embed
 
 
