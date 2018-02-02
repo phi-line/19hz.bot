e9a3a42b60e033de09b354226e5088273f239b61
phi-line <phi.liney@gmail.com>
2018-02-01 16:39:47 -0800

    added configs

--- data.py
+++ data.py
@@ -3,17 +3,12 @@ import requests
 from urllib import parse
 import json
 
-DATA_PATH = 'data/'  # local path
-DATA_URL = 'https://19hz.info/'  # online base
-DATA_FILES = ['events_BayArea']  # filename
-DATA_HEADERS = ["date", "name", "genre", 'location', 'time', 'price',
-                'ages', 'promoter', 'url1', 'url2', 'datetime']
+from data import config as c
 
-def fetch_data():
-    for u in DATA_FILES:
-        csv_url = parse.urljoin(DATA_URL, u + '.csv')
-        csv_local_path = parse.urljoin(DATA_PATH, u + '.csv')
-        json_local_path = parse.urljoin(DATA_PATH, u + '.json')
+def update_data():
+    for u in c.DATA_FILES:
+        csv_url = parse.urljoin(c.DATA_URL, u + '.csv')
+        json_local_path = parse.urljoin(c.DATA_PATH, u + '.json')
 
         data = requests.get(csv_url)
 
@@ -21,13 +16,21 @@ def fetch_data():
         reader = csv.reader(data.text.splitlines())
 
         for row in reader:
-            entry = dict(zip(DATA_HEADERS, row))
+            entry = dict(zip(c.DATA_HEADERS, row))
             entry_list.append(entry)
 
         json.dump(entry_list, open(json_local_path, 'w'), indent=4)
-        # data = json.load(open(json_local_path))
     return
+
+def fetch_data(location):
+    try:
+        json_local_path = parse.urljoin(c.DATA_PATH, location + '.json')
+        data = json.load(open(json_local_path))
+        return data
+    except FileNotFoundError as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    fetch_data()
+    update_data()
+    print(fetch_data(location="events_BayArea"))
---
+++ data/config.py
@@ -0,0 +1,6 @@
+DATA_PATH = 'data/'  # local path
+DATA_URL = 'https://19hz.info/'  # online base
+DATA_FILES = ['events_BayArea', 'events_LosAngeles', 'events_Atlanta',
+              'events_Texas', 'events_Miami', 'events_Massachusetts']  # filename
+DATA_HEADERS = ["date", "name", "genre", 'location', 'time', 'price',
+                'ages', 'promoter', 'url1', 'url2', 'datetime']
